7c5c6dab59f362a06665d70da59e3100f0f22dec
rongying <xxx5062133@gmail.com>
2021-03-25 17:40:43 +0800

    update

--- test/p01.py
+++ test/p01.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
 
-video = VideoFileClip("media/501A.wmv").subclip(50,60)
+video = VideoFileClip("media/IMG_0074.MOV").subclip(0,10)
 
 # Make the text. Many more options are available.
 txt_clip = ( TextClip("501A Story",fontsize=70,color='white')
@@ -9,3 +9,4 @@ txt_clip = ( TextClip("501A Story",fontsize=70,color='white')
 
 result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
 result.write_videofile("media/newStory.mp4",fps=25) # Many options...
+
