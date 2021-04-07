7be51b9162442656da0a7528826563fd9984d6bc
yaya <julia0988006808@gmail.com>
2021-04-07 12:25:38 +0800

    update

--- test/p02.py
+++ test/p02.py
@@ -1,9 +1,9 @@
 from moviepy.editor import *
 
-video1 = VideoFileClip("media/501A.wmv").subclip(10,20)
-video2 = VideoFileClip("media/501A.wmv").subclip(20,30)
-video3 = VideoFileClip("media/501A.wmv").subclip(30,40)
-video4 = VideoFileClip("media/501A.wmv").subclip(40,50)
+video1 = VideoFileClip("media/0001 (1).mp4").subclip(0,10)
+video2 = VideoFileClip("media/0001 (1).mp4").subclip(0,10)
+video3 = VideoFileClip("media//0001 (1).mp4").subclip(0,10)
+video4 = VideoFileClip("media//0001 (1).mp4").subclip(0,10)
 
 final_clip = clips_array([[video1, video2],
                           [video3, video4]])
---
+++ test/p04.py
@@ -0,0 +1,31 @@
+from moviepy.editor import *
+from moviepy.video.tools.segmenting import findObjects
+
+# Load the image specifying the regions.
+# im = ImageClip("../../ultracompositing/motif.png")
+
+# Loacate the regions, return a list of ImageClips
+regions = findObjects(im)
+
+
+# Load 7 clips from the US National Parks. Public Domain :D
+clips = [VideoFileClip(n, audio=False).subclip(18,22) for n in
+     [ "media/0001 (1).mp4",
+      "media/0001 (2).mp4",
+      "media/0001 (3).mp4",
+      "media/0001 (4).mp4",
+      "media/0001 (5).mp4",
+      "media/0001 (6).mp4",
+      "media/0001 (7).mp4"]]
+
+# fit each clip into its region
+comp_clips =  [c.resize(r.size)
+                .set_mask(r.mask)
+                .set_pos(r.screenpos)
+               for c,r in zip(clips,regions)]
+
+cc = CompositeVideoClip(comp_clips,im.size)
+cc.resize(0.6).write_videofile("../../composition.mp4")
+
+# Note that this particular composition takes quite a long time of
+# rendering (about 20s on my computer for just 4s of video).
