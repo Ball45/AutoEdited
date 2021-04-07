88e5246a82e36c9e3c3eac16ad9b5b0c61ecb1a5
yaya <julia0988006808@gmail.com>
2021-04-07 14:18:59 +0800

    update

--- test/p02.py
+++ test/p02.py
@@ -1,9 +1,9 @@
 from moviepy.editor import *
-
-video1 = VideoFileClip("media/0001 (1).mp4").subclip(0,10)
-video2 = VideoFileClip("media/0001 (1).mp4").subclip(0,10)
-video3 = VideoFileClip("media//0001 (1).mp4").subclip(0,10)
-video4 = VideoFileClip("media//0001 (1).mp4").subclip(0,10)
+import os
+video1 = VideoFileClip("media/0001 (1).mp4").subclip(0, 8)
+video2 = VideoFileClip("media/0001 (1).mp4").subclip(0, 8)
+video3 = VideoFileClip("media/0001 (1).mp4").subclip(0, 8)
+video4 = VideoFileClip("media/0001 (1).mp4").subclip(0, 8)
 
 final_clip = clips_array([[video1, video2],
                           [video3, video4]])
