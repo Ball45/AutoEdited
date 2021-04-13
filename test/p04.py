5190c4a229fd67558b63f0d3d02ca40fa808a289
yaya <julia0988006808@gmail.com>
2021-04-13 17:13:59 +0800

    update

--- test/p04.py
+++ test/p04.py
@@ -1,31 +1,7 @@
-from moviepy.editor import *
-from moviepy.video.tools.segmenting import findObjects
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-# Load the image specifying the regions.
-# im = ImageClip("../../ultracompositing/motif.png")
+clip1 = VideoFileClip("media/0001 (3).mp4").subclip(0, 5)
+clip2 = VideoFileClip("media/0001 (3).mp4").subclip(0, 5)
 
-# Loacate the regions, return a list of ImageClips
-regions = findObjects(im)
-
-
-# Load 7 clips from the US National Parks. Public Domain :D
-clips = [VideoFileClip(n, audio=False).subclip(0, 5) for n in
-         ["media/0001 (1).mp4",
-          "media/0001 (2).mp4",
-          "media/0001 (3).mp4",
-          "media/0001 (4).mp4",
-          "media/0001 (5).mp4",
-          "media/0001 (6).mp4",
-          "media/0001 (7).mp4"]]
-
-# fit each clip into its region
-comp_clips = [c.resize(r.size)
-              .set_mask(r.mask)
-              .set_pos(r.screenpos)
-              for c, r in zip(clips, regions)]
-
-cc = CompositeVideoClip(comp_clips, im.size)
-cc.resize(0.6).write_videofile("media/composition.mp4")
-
-# Note that this particular composition takes quite a long time of
-# rendering (about 20s on my computer for just 4s of video).
+finalclip = concatenate_videoclips([clip1, clip2])
+finalclip.write_videofile("media/out.mp4")  # 寫出檔案out.mp4
