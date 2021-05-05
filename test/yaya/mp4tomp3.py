a626f24ad10001a2a4e2e8ebc9f025528e8f9f6f
yaya <julia0988006808@gmail.com>
2021-05-05 20:15:03 +0800

    update

--- test/yaya/mp4tomp3.py
+++ test/yaya/mp4tomp3.py
@@ -1,5 +1,5 @@
 import os
 
-mp4file = "media\\train.mp4"
-mp3file = "media\\railway.mp3"
+mp4file = "media\\shallow.mp4"
+mp3file = "media\\guitar.mp3"
 os.system("ffmpeg -i "+mp4file+" "+mp3file)
---
+++ test/yaya/silence.py
@@ -0,0 +1,5 @@
+import os
+
+
+jls_extract_var = time2.mp3
+os.system("ffmpeg - i " + jls_extraci_varmp3+" - af silencedetect - f null")
