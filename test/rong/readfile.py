7f81a87a02a2fddfe336de8ed69b3f7bb57f272c
yaya <julia0988006808@gmail.com>
2021-05-27 01:28:30 +0800

    update

--- test/rong/readfile.py
+++ test/rong/readfile.py
@@ -1,37 +1,23 @@
 import speech_recognition as sr
 r = sr.Recognizer()
-<<<<<<< HEAD
 with sr.AudioFile("media\\test1.wav") as source:
-    audio = r.record(source)
-
-try:
-    s = r.recognize_google(audio_data=audio, key=None,
-                           language="zh-TW", show_all=True)
-    print("Text: "+s)
-    if '剪接' in s:
-=======
-with sr.AudioFile("media/testb.wav") as source:
     audio = r.record(source)
 
 try:
     instruction = r.recognize_google(audio, language="zh-TW", show_all=True)
     #instruction=r.recognize_google(audio_data=audio, key=None, language="zh-TW")
-    print("Text: ",instruction)
-    
-    if "剪接" in instruction :
->>>>>>> 63ac1d0d87e96201d588c63afa1ffcb841a22d8e
+    print("Text: ", instruction)
+
+    if "剪接" in instruction:
         print("剪接")
     else:
         print('pass')
-<<<<<<< HEAD
 except Exception as e:
     print("Exception: "+str(e))
-=======
-#except Exception as e:
+# except Exception as e:
  #   print("Exception: "+str(e))
 
 except r.UnknowValueError:
-        Text = "無法翻譯"
+    Text = "無法翻譯"
 except sr.RequestError as e:
-        Text = "無法翻譯{0}".format(e)
->>>>>>> 63ac1d0d87e96201d588c63afa1ffcb841a22d8e
+    Text = "無法翻譯{0}".format(e)
