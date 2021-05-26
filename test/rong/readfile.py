afeb1ad0715bcd6ecb6cc72fcb5bdb5c7e27313e
rongying <xxx5062133@gmail.com>
2021-05-26 16:31:59 +0800

    update SRenglish

--- test/rong/readfile.py
+++ test/rong/readfile.py
@@ -4,7 +4,7 @@ with sr.AudioFile("media/testa.wav") as source:
     audio = r.record(source)
 
 try:
-    s = r.recognize_google(audio, language="zh-TW")
+    s=r.recognize_google(audio_data=audio, key=None, language="zh-TW", show_all=True)
     print("Text: "+s)
     if '剪接' in s :
         print("剪接")
