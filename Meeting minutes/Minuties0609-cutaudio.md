27d98eed0a58cce8d94461ad05a9b49d6b883557
rongying <xxx5062133@gmail.com>
2021-06-09 16:32:46 +0800

    update

---
+++ Meeting minutes/Minuties0609-cutaudio.md
@@ -0,0 +1,3 @@
+## cutaudio
+[reference1](https://twgreatdaily.com/JT0EhXEBfwtFQPkd643n.html)
+[reference2](https://blog.csdn.net/ZT7524/article/details/104280794)
--- test/rong/comparemp3.py
+++ test/rong/comparemp3.py
@@ -1,9 +1,27 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
 
-from compare_mp3 import compare
+sampling_freq, audio = wavfile.read(r"C:\Windows\media\Windows Background.wav")   # 讀取檔案
 
-songmp31 = "media/testa.mp3"
-songmp32 = "media/testb.mp3"
+audio = audio / np.max(audio)   # 歸一化，標準化
 
-if compare(songmp31, songmp32):
-    print('Files contain the same audio')
+# 應用傅立葉變換
+fft_signal = np.fft.fft(audio)
+print(fft_signal)
+# [-0.04022912+0.j         -0.04068997-0.00052721j -0.03933007-0.00448355j
+#  ... -0.03947908+0.00298096j -0.03933007+0.00448355j -0.04068997+0.00052721j]
 
+fft_signal = abs(fft_signal)
+print(fft_signal)
+# [0.04022912 0.04069339 0.0395848  ... 0.08001755 0.09203427 0.12889393]
+
+# 建立時間軸
+Freq = np.arange(0, len(fft_signal))
+
+# 繪製語音訊號的
+plt.figure()
+plt.plot(Freq, fft_signal, color='blue')
+plt.xlabel('Freq (in kHz)')
+plt.ylabel('Amplitude')
+plt.show()
