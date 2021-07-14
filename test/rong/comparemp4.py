61799ede7c825967faaf59e4df3d1dcdcdc9ecb5
rongying <xxx5062133@gmail.com>
2021-07-14 21:26:18 +0800

    update comparemp4

--- test/rong/comparemp4.py
+++ test/rong/comparemp4.py
@@ -1,12 +1,12 @@
+
 from moviepy.editor import VideoFileClip
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
 clip = VideoFileClip("media/tryf.mp4") 
-clip1 = clip.subclip(0, 3) 
-clip2 = clip.subclip(7, 9)
 count=0
+fps=30
 sum=0
 summ=0
 new_frame = [] 
@@ -16,41 +16,34 @@ def square(a):
         return i**2
 
 for frames in clip.iter_frames():
-    #print (frames.shape)
     gray = cv.cvtColor(frames, cv.COLOR_BGR2GRAY)
     #cv.imshow("gray", gray)
-    #print (gray.shape)
     new_frame.append(gray)
     count+=1
     key = cv.waitKey(1)
     if key == ord("q"):
-        break
-    
+        break   
 print (count)
+
 print('new_frame[0] : ',new_frame[0])
-print('new_frame[4] : ',new_frame[4])
-#print('new_frame[0]^2 : ',square(new_frame[0]))
-#print('new_frame[4] : ',square(new_frame[4]))
+print('new_frame[270] : ',new_frame[270])
+
 print (new_frame[0]- new_frame[4])
-print (square(new_frame[4]- new_frame[0]))
-for i in square(new_frame[4]- new_frame[0]) :
-    sum=sum+i
-print(sum)
+print (square(new_frame[270]- new_frame[0]))
+
+#平方和
+#for i in square(new_frame[270]- new_frame[0]) :
+#    sum=sum+i
+#print(sum)
+
 List_length = len(new_frame)
-print(List_length,len(gray),len(gray[0]))
+print(List_length,len(new_frame[0]),len(gray),len(gray[0]),len(square(new_frame[270]- new_frame[0])))
+time=len(new_frame)/fps
 
-for i in range(10):
-    for j in square(new_frame[i]- new_frame[7]) :
-        summ=summ+j
-    print('t : ',i,' - 7 =', summ, '\n')
+#比較第t秒和第7秒的frames，一秒鐘有30個frame(fps=30)
+for t in range(int(time)):
+    for k in range(fps):
+        for j in square(new_frame[t+k]- new_frame[7+k]) :
+            summ=summ+j
+    print('t : ',t,' - 7 =', summ, '\n')
     summ=0
-
-
-
-#for i in range :
- #   sum=sum+new_frame[i]*new_frame[i]
-
-#print(new_frame)
-#print((new_frame[3]-new_frame[7])*(new_frame[3]-new_frame[7]))
-#print([frames[0].max() for frames in clip.get_frame(3)])
-
