ba3f89e752929580fbaf0569227a626fc06fa473
rongying <xxx5062133@gmail.com>
2021-07-20 18:48:05 +0800

    update comparemp4

--- test/movie editor/comparemp4.py
+++ test/movie editor/comparemp4.py
@@ -2,51 +2,63 @@
 from moviepy.editor import VideoFileClip
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 
-clip = VideoFileClip("media\\airplane.mp4")
+
+clip = VideoFileClip("media/try720.MOV")
 count = 0
 fps = 30
 sum = 0
 summ = 0
+summm=0
 new_frame = []
+cutpoint=21
 
 
-def square(a):
-    for i in a:
-        return i**2
-
-
+#轉灰階
 for frames in clip.iter_frames():
     gray = cv.cvtColor(frames, cv.COLOR_BGR2GRAY)
-    #cv.imshow("gray", gray)
+    #cv.imshow("gray", gray) #播放灰階影片
     new_frame.append(gray)
     count += 1
     key = cv.waitKey(1)
     if key == ord("q"):
         break
 print(count)
+time = count/fps
 
-print('new_frame[0] : ', new_frame[0])
-print('new_frame[270] : ', new_frame[270])
+# frame size
+print('frame size : W =', len(gray[0]), 'H =', len(new_frame[0]))
 
-print(new_frame[0] - new_frame[4])
-print(square(new_frame[270] - new_frame[0]))
+#測試加減、平方有沒有錯
+#print('new_frame[0] : ', new_frame[0])
+#print('new_frame[270] : ', new_frame[270])
 
-# 平方和
-# for i in square(new_frame[270]- new_frame[0]) :
-#    sum=sum+i
-# print(sum)
+#測試減法
+#xx=new_frame[270] - new_frame[0]
+#print(xx)
 
-List_length = len(new_frame)
-print(List_length, len(new_frame[0]), len(gray), len(
-    gray[0]), len(square(new_frame[270] - new_frame[0])))
-time = len(new_frame)/fps
+#測試平方
+#yy=np.square(new_frame[0] - new_frame[270])
+#print(yy)
+#print(len(yy),len(yy[0]),len(xx),len(xx[2]))
 
-# 比較第t秒和第7秒的frames，一秒鐘有30個frame(fps=30)
-for t in range(int(time)):
-    for k in range(fps):
-        for j in square(new_frame[t+k] - new_frame[7+k]):
-            summ = summ+j
-    print('t : ', t, ' - 7 =', summ, '\n')
-    summ = 0
+
+min = 1000000
+# 比較第t秒和第cutpoint秒的frames，一秒鐘有30個frame(fps=30)
+for cutpoint in range(21,25) :
+    for t in range(11):
+        for k in range(fps):
+            for i in np.square(new_frame[t+k] - new_frame[cutpoint+k]):
+                summ = summ + i
+        for j in summ :
+            summm = summm + j    
+        print('t : ', t, ' - ',cutpoint,' =', summm, '\n')
+        if min>summm:
+            t1=t
+            t2=cutpoint
+            min=summm
+            
+        summ = 0
+        summm = 0
+#輸出t1和t2最相近
+print (t1,t2,min)
