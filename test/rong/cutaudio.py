0d2abbd16f985a5879384fe15f410c2fbc48b94f
rongying <xxx5062133@gmail.com>
2021-07-31 01:29:53 +0800

    update

--- test/rong/cutaudio.py
+++ test/rong/cutaudio.py
@@ -7,17 +7,18 @@ file1_name = r"media/testc.wav"
 file2_name = r"media/testc.wav"
 sound = AudioSegment.from_wav(file1_name)
 #sound2 = AudioSegment.from_wav(file2_name)
-
+a=14.3
+b=20.9
 # 取33秒到70秒間的片段
-sound1 = sound[14.3*SECOND:20.9*SECOND]
+sound1 = sound[a*SECOND:b*SECOND]
 sound2 = sound[24.75*SECOND:27.75*SECOND]
 
 # 拼接两个音频文件
-finSound = sound1 + sound2
+#finSound = sound1 + sound2
 print("save_path:")
 
 # 導出音樂
-finSound.export("media/finsound.mp3")
+sound1.export("media/finsound.wav")
 
 
 
