909d5e8643db511707368ed40cb527735235f698
rongying <xxx5062133@gmail.com>
2021-08-17 21:52:39 +0800

    update

--- test/rong/autosub/merge_sub_with_video.py
+++ test/rong/autosub/merge_sub_with_video.py
@@ -5,6 +5,3 @@ subtitles = "'media/IMG_958999.srt'"
 force_style = ":force_style='Fontsize=24'"
 outputVideo = "media/IMG_958999out817.mp4"
 os.system("ffmpeg -i "+sourceVideo+" -i "+subtitles+" -c:s mov_text -c:v copy -c:a copy "+outputVideo)
-#ffmpeg -i program/video/media/IMG_95899.mp4 -vf "subtitles='program/video/media/IMG_9589.srt'" program/video/media/IMG_95899out817.mp4
-#ffmpeg -f concat -safe 0 -i mp4files.txt -c copy ConcatenatedVideo.MP4
-#ffmpeg -i inputfile.mp4 -i subtitles.srt -c:s mov_text -c:v copy -c:a copy outputfile.mp4"
