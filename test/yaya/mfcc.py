1a8655fe86e3d0265d8a14ab03104a752a1ec7a9
shunyaya <julia0988006808@gmail.com>
2021-10-26 21:01:07 +0800

    update

--- test/yaya/mfcc.py
+++ test/yaya/mfcc.py
@@ -1,4 +1,9 @@
-import soundfile as sf
-
-data, samplerate = sf.read('media\\2021-08-27.wav')
-sf.write('new_file.flac', data, samplerate)
+# import librosa
+import librosa.display 
+import matplotlib.pyplot as plt
+x , sr = librosa.load("media\\2021-08-27.wav", sr=8000)
+X = librosa.stft(x)
+Xdb = librosa.amplitude_to_db(abs(X))   # 把幅度轉成分貝格式
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+plt.colorbar()
