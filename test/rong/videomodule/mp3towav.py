4159c94866e795536b8aa1b4f4737aa08a00407b
rongying <xxx5062133@gmail.com>
2021-11-30 17:10:17 +0800

    playvideo

--- test/rong/videomodule/mp3towav.py
+++ test/rong/videomodule/mp3towav.py
@@ -2,3 +2,13 @@ import os
 songmp3 = "media/testc.mp3"
 songwav = "media/testc.wav"
 os.system("ffmpeg -i "+songmp3+" "+songwav)
+
+
+songm4a = "media/testc.m4a"
+songmp3 = "media/testc.mp3"
+os.system("ffmpeg -i "+songm4a+" -acodec libmp3lame -ab 256k "+songmp3)
+
+
+videomp4 = "media/testc.m4a"
+videoavi = "media/testc.mp3"
+os.system("ffmpeg -i "+ videomp4 + " -c copy -map 0 " + videoavi)
